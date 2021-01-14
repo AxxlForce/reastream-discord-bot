53f53aee38c4c54b745ddf4c3fd707f1909a2115
Alexander Stöcker <alexander_stoecker@web.de>
2021-01-14 01:14:30 +0100

    trash everything we don't need, improve build scrip to shrink binary size

--- build.sh
+++ build.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
-rm -rf node_modules dist
-npm ci
-npm run build
-nexe dist/src/main.js -r "node_modules/**/*" -t windows-x64-12.16.2 -o reastream-discard-bot.exe
-nexe dist/src/main.js -r "node_modules/**/*" -t mac-x64-12.16.2 -o reastream-discard-bot-mac
-nexe dist/src/main.js -r "node_modules/**/*" -t linux-x64-12.16.2 -o reastream-discard-bot-linux
+rm -rf node_modules dist build
 
+# get all dependencies to build app and then remove them as they include node dev dependencies
+npm ci && npm run build
+rm -rf node_modules
+
+# only install node production dependencies
+npm install --production
+
+# install nexe to different folder so it doesn't get bundled
+npm install --prefix ./tmp nexe
+
+# build binariesactual binaries using only the node production dependencies
+node_modules='node_modules/**/*.{js,wasm}'
+mkdir build
+./tmp/node_modules/.bin/nexe dist/main.js -r $node_modules -t windows-x64-12.16.2 -o build/reastream-discard-bot.exe
+./tmp/node_modules/.bin/nexe dist/main.js -r $node_modules -t mac-x64-12.16.2 -o build/reastream-discard-bot-mac
+./tmp/node_modules/.bin/nexe dist/main.js -r $node_modules -t linux-x64-12.16.2 -o build/reastream-discard-bot-linux
+
+# cleanup
+#rm -rf node_modules tmp
