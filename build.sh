3da473a3f7d6ed8da2e3dc8b5b0204e679b2c13e
Alexander Stöcker <alexander_stoecker@web.de>
2021-01-21 00:11:03 +0100

    fix build script which was missing json files in dependencies

--- build.sh
+++ build.sh
@@ -12,12 +12,12 @@ npm install --production
 # install nexe to different folder so it doesn't get bundled
 npm install --prefix ./tmp nexe
 
-# build binariesactual binaries using only the node production dependencies
-node_modules='node_modules/**/*.{js,wasm}'
+# build binaries using only the node production dependencies
+node_modules='node_modules/**/*.{js,json,wasm}'
 mkdir build
-./tmp/node_modules/.bin/nexe dist/main.js -r $node_modules -t windows-x64-12.16.2 -o build/reastream-discard-bot.exe
-./tmp/node_modules/.bin/nexe dist/main.js -r $node_modules -t mac-x64-12.16.2 -o build/reastream-discard-bot-mac
-./tmp/node_modules/.bin/nexe dist/main.js -r $node_modules -t linux-x64-12.16.2 -o build/reastream-discard-bot-linux
+./tmp/node_modules/.bin/nexe dist/main.js -r $node_modules -t windows-x64-10.9.0 -o build/reastream-discard-bot.exe
+./tmp/node_modules/.bin/nexe dist/main.js -r $node_modules -t mac-x64-10.9.0 -o build/reastream-discard-bot-mac
+./tmp/node_modules/.bin/nexe dist/main.js -r $node_modules -t linux-x64-10.9.0 -o build/reastream-discard-bot-linux
 
 # cleanup
-#rm -rf node_modules tmp
+rm -rf node_modules tmp
